350a937cb59762b52e20e83fa04ee30d593c6e3e
jakhamma <woodybrendan@hotmail.com>
2017-01-03 17:34:13 -0800

    first commit

---
+++ StanfordCalculator/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  StanfordCalculator
+//
+//  Created by brendan woods on 2017-01-02.
+//  Copyright © 2017 brendan woods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping
+        {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+            
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    private var displayValue:Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton)
+    {
+        if userIsInTheMiddleOfTyping
+        {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathmaticalSymbol = sender.currentTitle
+        {
+            brain.performOperation(mathematicalSymbol: mathmaticalSymbol)
+            displayValue = brain.result
+        }
+    }
+    
+    
+}
+
